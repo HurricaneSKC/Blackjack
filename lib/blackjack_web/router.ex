da26f005af8cc1da765709607b3516e82a5092ec
Sanjay Chopra <sanjay@fostr.io>
2021-03-20 19:57:09 +0000

    redirect to client side route

--- lib/blackjack_web/router.ex
+++ lib/blackjack_web/router.ex
@@ -16,7 +16,7 @@ defmodule BlackjackWeb.Router do
   scope "/", BlackjackWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    get("/*path", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
